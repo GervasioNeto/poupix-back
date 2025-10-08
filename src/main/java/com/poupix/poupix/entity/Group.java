4993f5cf38dafc1e605048da38ea30da91fc051e
Gervasio Neto <gervasiolopesn@gmail.com>
2025-10-08 15:10:12 -0300

    feat: added UUIID for group

--- src/main/java/com/poupix/poupix/entity/Group.java
+++ src/main/java/com/poupix/poupix/entity/Group.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @Entity
 @Table(name = "groups")
@@ -15,6 +16,9 @@ public class Group {
     private Long id;
     private String name;
     private String description;
+
+    @Column(unique = true, updatable = false, nullable = false)
+    private String uuid = UUID.randomUUID().toString();
 
     @ManyToMany
     @JoinTable(
@@ -29,11 +33,12 @@ public class Group {
 
     public Group() {}
 
-    public Group(Long id, String name, String description, List<User> users) {
+    public Group(Long id, String name, String description, List<User> users, String uuid) {
         this.id = id;
         this.name = name;
         this.description = description;
         this.users = users;
+        this.uuid = uuid;
     }
 
     public Long getId() {
@@ -74,5 +79,13 @@ public class Group {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
     }
 }
